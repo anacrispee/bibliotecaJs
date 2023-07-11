55ddbae5daec1e8cdf54850bcf933927d020ad8c
Ana Cristina <anacrispee@gmail.com>
2023-07-11 12:34:56 -0300

    update code

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 /* Importando o PrismaClient constructor do módulo do Prisma do Node e o instanciando */
-const app = require('./app')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 /* Cria uma função assíncrona onde alocará as manipulações com o banco de dados. */
 async function main() {
-  // ... you will write your Prisma Client queries here
-  async function main() {
+  //Aqui dentro dessa função assíncrona você vai colocar suas querys.
+    /* SELECT * FROM categorias: */
     const allCategorias = await prisma.categoria.findMany()
     console.log(allCategorias)
-  }
 }
 
 /* Chama a função "main" criada e a fecha quando finalizada a manipulação com BD. ("process.exit"). */
