e820059ed13a5584da89a7ac3e7ee6a450b1d039
Ana Cristina <anacrispee@gmail.com>
2023-07-17 23:10:14 -0300

    update

--- public/index.js
+++ public/index.js
@@ -4,9 +4,6 @@ async function biblioteca() {
     const responseCategorias = await fetch('/categorias');
     const categorias = await responseCategorias.json();
 
-    const responseLivros = await fetch('/livros');
-    const livros = await responseLivros.json();
-
     const tabela = document.getElementById('tabela-categorias');
     tabela.innerHTML = '';
 
@@ -34,69 +31,30 @@ async function biblioteca() {
     categorias.forEach(categoria => {
 
         const linha = document.createElement('tr');
+        tabela.appendChild(linha);
+
         const colunaId = document.createElement('td');
-        const colunaNome = document.createElement('td');
-        const colunaBtn = document.createElement('td');
-
-        //Table rows content:
         colunaId.textContent = `${categoria.id_categoria}`;
-        colunaNome.textContent = `${categoria.nome_categoria}`;
+        linha.appendChild(colunaId);
 
-        //Botões de cada categoria:
+        const colunaNome = document.createElement('td');
+        colunaNome.textContent = `${categoria.nome_categoria}`;
+        linha.appendChild(colunaNome);
+
+        const colunaBtn = document.createElement('td');
         const btnShow = document.createElement('button');
         const btnEdit = document.createElement('button');
         const btnDelete = document.createElement('button');
         btnShow.textContent = 'show';
         btnEdit.textContent = 'edit';
         btnDelete.textContent = 'delete';
-
-        //Anexando os elementos:
         colunaBtn.appendChild(btnShow);
         colunaBtn.appendChild(btnEdit);
         colunaBtn.appendChild(btnDelete);
-
-        linha.appendChild(colunaId);
-        linha.appendChild(colunaNome);
         linha.appendChild(colunaBtn);
-        tabela.appendChild(linha);
 
-        //Tabela livros - Listagem de livros por categoria:
-        const linhaLivros = document.createElement('tr');
-        colunaNome.appendChild(linhaLivros);
+        const tableLivro = document.createElement('table');
+        colunaNome.appendChild(tableLivro);
 
-        const tabelaLivros = document.createElement('table');
-        linhaLivros.appendChild(tabelaLivros);
-        tabelaLivros.style.border = '1px solid';
-
-        const trTabelaLivros = document.createElement('tr');
-        tabelaLivros.appendChild(trTabelaLivros);
-
-        const thLivrosID = document.createElement('th');
-        trTabelaLivros.appendChild(thLivrosID);
-        thLivrosID.textContent = 'id';
-
-        const thLivrosNome = document.createElement('th');
-        trTabelaLivros.appendChild(thLivrosNome);
-        thLivrosNome.textContent = 'nome livro';
-
-        const thLivroAutor = document.createElement('th');
-        trTabelaLivros.appendChild(thLivroAutor);
-        thLivroAutor.textContent = 'autor livro';
-
-        livros.forEach(livro => {
-            const linhaLivro = document.createElement('tr');
-            const colIdLivro = document.createElement('td');
-            const colNomeLivro = document.createElement('td');
-            const colAutorLivro = document.createElement('td');
-
-            colIdLivro.textContent = `${livro.id_livro}`;
-            colNomeLivro.textContent = `${livro.nome_livro}`;
-            colAutorLivro.textContent = `${livro.autor_livro}`;
-
-            linhaLivro.appendChild(colIdLivro);
-            linhaLivro.appendChild(colNomeLivro);
-            linhaLivro.appendChild(colAutorLivro);
-            tabelaLivros.appendChild(linhaLivro);
-        })
     })
 }
--- routes/livros.js
+++ routes/livros.js
@@ -4,9 +4,19 @@ const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
 //Rota pra listar os livros:
-router.get('/', async (req, res) => {
+/*router.get('/', async (req, res) => {
     const livros = await prisma.livros.findMany();
     res.json(livros);
-});
+});*/
+
+router.get('/:id_categoria', async (req, res) => {
+    const {id_categoria} = req.params;
+    const livros = await prisma.livros.findMany({
+        where: {
+            id_categoria: parseInt(id_categoria)
+        }
+    })
+    res.json(livros);
+})
 
 module.exports = router;
